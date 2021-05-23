096cea8a8b0976e1a55049a344b39b08ebbcc4be
devTam <tammy.batubo@gmail.com>
2021-05-24 00:03:54 +0100

    refactor routers

---
+++ src/routers/user.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const User = require("../models/user");
+const router = new express.Router();
+
+router.post("/users", async (req, res) => {
+    const user = new User(req.body);
+  
+    try {
+      await user.save();
+      res.status(201).send(user);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
+  
+  router.get("/users", async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.send(users);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+  
+  router.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+  
+    try {
+      const user = await User.findById(_id);
+      if (!user) {
+        res.status(404).send();
+      }
+      res.send(user);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+  
+  router.patch("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    const body = req.body;
+  
+    const updates = Object.keys(body);
+    const allowedUpdates = ["name", "email", "password", "age"];
+    const isValidUpdate = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
+  
+    if (!isValidUpdate) {
+      return res.status(400).send({ error: "Update not allowed!" });
+    }
+  
+    try {
+      const user = await User.findByIdAndUpdate(_id, body, {
+        new: true,
+        runValidators: true,
+      });
+  
+      if (!user) {
+        res.status(404).send();
+      }
+      res.send(user);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
+  
+  router.delete("/users/:id", async (req, res) => {
+  
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+  
+      if (!user) {
+        return res.status(404).send();
+      }
+  
+      res.send(user);
+  
+    } catch (e) {
+      res.status(500).send();
+    }
+  });
+
+module.exports = router; 
