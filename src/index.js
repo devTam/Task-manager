4edde10bf813b4d45689ab1813bf6597c64f2c2a
devTam <tammy.batubo@gmail.com>
2021-05-23 15:50:14 +0100

    add read endpoints for users and task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 // process.env.PORT is the port heroku uses for deployment
@@ -9,26 +9,83 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body);
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
 
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((e) => {
-        res.status(400).send(e)
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
     })
-})
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
     })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on port: ${port}`)
-})
+  console.log(`Server is listening on port: ${port}`);
+});
